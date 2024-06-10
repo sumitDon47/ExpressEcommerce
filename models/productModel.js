963acb66b948ebba85b3f400be91742b4ef3e5ed
Sumit <sumit@pop-os.localdomain>
2024-06-10 14:57:25 +0545

    Add product model table

---
+++ models/productModel.js
@@ -0,0 +1,39 @@
+const mongoose = requirer('mongoose')
+const {ObjectId} = mongoose.Schema
+
+//object is used t link data between different collection (table)
+const productSchema = new mongoose.Shema({
+    product_name:{
+        type:String,
+        required:true,
+        trim: true
+    },
+    product_price:{
+        type:Number,
+        required:true
+    },
+    countInStock:{
+        type:Number,
+        required:true
+    },
+    product_description:{
+        type:String,
+        required:true
+    },
+    product_image:{
+        type:String,
+        required:true
+    },
+    product_rating:{
+        type:Number,
+        default:0,
+        max:5
+    },
+    category:{
+        type:ObjectId,
+        required:true,
+        ref:'Category'
+    }
+}, {timestamp:true})
+
+module.exports = mongoose.model('Product', productSchema)

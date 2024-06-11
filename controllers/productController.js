76e9ddf47570949bc87867a94b1016694fb7b299
Sumit <sumit@pop-os.localdomain>
2024-06-11 16:43:02 +0545

    Delete product

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,7 +12,62 @@ exports.postProduct = async(req, res) => {
     })
     product = await product.save()
     if(!product){
-        return res.status(400).json({error:'something went wromg'})   
+        return res.status(400).json({error:'something went wrong'})   
     }
     res.send(product)
+}
+
+//to show all the products
+exports.productList = async(req,res) => {
+    const product = await Product.find()
+    if(!product){
+        return res.status(400).json({error:'something went wrong'})   
+    }
+    res.send(product)
+}
+
+//product details
+exports.productDetails = async (req, res)=>{
+    const product = await Product.findById(req.params.id)
+    .populate('category','category_name')
+    if(!product){
+        return res.status(400).json({error:'something went wrong'})   
+    }
+    res.send(product)
+}
+
+//to update product
+exports.updateProduct = async(req, res)=>{
+    const product= await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            product_name:req.body.product_name,
+            product_price:req.body.product_price,
+            countInStock:req.body.countInStock,
+            product_description:req.body.product_description,
+            product_image:req.body.product_image,
+            category:req.body.category 
+        },
+        {new:true}
+    )
+    if(!product){
+        return res.status(400).json({error:'something went wrong'})   
+    }
+    res.send(product)
+}
+
+//delete product
+exports.deleteProduct = (req,res)=>{
+    Prodcut.findByIdAndDelete(req.param.id)
+    .then(product=>{
+        if(!product){
+            return res.status(404).json({error:'product with that id not found'})
+        }
+        else{
+            return res.status(200).json({message:'product deleted'})
+        }
+    })
+    .catch(err=>{
+        return res.status(400).json({error:err})
+    })
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { postProduct } = require('../controllers/productController')
+const { postProduct, productList, productDetails, updateProduct, deleteProduct } = require('../controllers/productController')
 const router = express.Router()
 
 router.post('/postproduct',postProduct)
-
-
-
+router.get('/productlist', productList)
+router.get('/productdetails/:id',productDetails)
+router.put('/updateproduct/:id', updateProduct)
+router.delete('./deleteproduct/:id',deleteProduct)
 
 module.exports = router

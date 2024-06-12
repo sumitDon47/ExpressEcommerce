0bd016f1c3ab145be03093acb86fb4824e8422ef
Sumit <sumit@pop-os.localdomain>
2024-06-12 13:22:12 +0545

    Add fileupload file

---
+++ middleware/fileupload.js
@@ -0,0 +1,45 @@
+const multer = require('multer')
+const fs = require('fs')  //file system help to find directory
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        let fileDestination = 'public/uploads/'
+        //check if directory exist 
+        if(!fs.existsSync(fileDestination)){
+            fs.mkdirSync(fileDestination,{recursive:true})
+            cb(null,fileDestination)
+        }
+        else{
+            cb(null,fileDestination)
+        }
+    },
+    filename:(req,file,cb)=>{
+        let filename = path.basename(file.originalname,path.extname(file.originalname))
+        //pat.basename('image/foo/abc.jpg)
+        //abc
+        let ext = path.extname(file.originalname)
+        //.jpg
+        cb(null, filename+'_'+DataTransfer.now()+ext)
+    }
+})
+
+let imageFilter = (req,file,cb)=>{
+    if(!file.originalname.match(/\.(jpg|png|jpeg|svg|JPG|PNG|JPEG|SVG|JFIF|GIF)$/)){
+        return cb(new Error('You can upload image file only'),false)
+    }
+    else{
+        cb(null,true)
+    }
+}
+
+const upload = multer({
+    storage:storage,
+    fileFilter:imageFilter,
+    limits:{
+        fileSize:3000000 //3mb
+    }
+})
+
+module.exports = upload
+

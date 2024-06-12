7a4dc82a5ef3a5cdbac778e11e5c27654e727538
Sumit <sumit@pop-os.localdomain>
2024-06-12 14:39:40 +0545

    validation

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,13 +1,15 @@
 const express = require ('express')
 const { testFunction, postCategory, categorylist, categoryList, categoryDetails, updateCategory, deleteCategory } = require('../controllers/categoryController')
 const router =  express.Router()
+const {categoryValidation, validation} = require('../validation/validator')
 
 router.get('/demo',testFunction)
-router.post('/postcategory', postCategory)
+router.post('/postcategory', categoryValidation,validation, postCategory)
 router.get('/categorylist', categoryList)
 router.get('/categorydetails/:id',categoryDetails)
 router.put('/updatecategory/:id',updateCategory)
 router.delete('/deletecategory/:id', deleteCategory)
+
 
 module.exports = router 
  
---
+++ validation/validator.js
@@ -0,0 +1,29 @@
+const{check,validationResult}=require('express-validator')
+
+exports.categoryValidation = [
+    check('category_name','category is required').notEmpty()
+    .isLength({min:3}).withMessage('category name must be of at least 3 characters')
+]
+
+exports.productValidation=[
+    check('product_name','product name is required').notEmpty()
+    .isLength({min:3}).withMessage('product name must be of at least 3 characters'),
+
+    check('product_price','price is required').notEmpty()
+    .isNumeric().withMessage('price must be a numeric'),
+    check('countInStock','stock is required').notEmpty()
+    .isNumeric().withMessage('price must be a numeric'),
+    check('prodct_description','description is required').notEmpty()
+    .isLength({min:20}).withMessage('Description muct be at least of 20 charactres or more'),
+    check('category','category is required').notEmpty()
+]
+
+exports.validation = (req,res,next)=>{
+    const errors = validationResult(req)
+    if(errors.isEmpty){
+        next()
+    }
+    else{
+        return res.status(400).json({error:errors.array()[0].msg})
+    }
+}

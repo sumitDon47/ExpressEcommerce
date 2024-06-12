cfc47a521bce7334d277c7d60227461243b1d035
Sumit <sumit@pop-os.localdomain>
2024-06-12 15:15:12 +0545

    add user model and install uuidv1

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const uuidv1 = require('uuidv1')
+const crypto = require('crypto')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+        trim:true
+    },
+    email: {
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    role: {
+        type:Number,
+        default:0
+    },
+    hashed_password:{
+        type:String,
+        required:true
+    },
+    salt:String,
+
+    isVerified:{
+        type:Boolean,
+        default:false
+    }
+
+},{timestamp:true})

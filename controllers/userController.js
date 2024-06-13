f091110e3c3611f25aee1aa9753f8d0248506e13
Sumit <sumit@pop-os.localdomain>
2024-06-13 14:40:47 +0545

    Add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+const User = require('../models/userModel')
+
+//to register user
+exports.postUser = async(req,res)=>{
+    let user = new User({
+        name:req.body.name,
+        email:req,body,email,
+        password:req.bosy.password
+    })
+    user = await user.save()
+    if(!user){
+        return res.status(400).json({error:'Unable to create account'})
+    }
+    res.send(user)
+}

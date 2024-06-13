264df3fa03e24be68e68ace783604bc3bb6a124c
Sumit <sumit@pop-os.localdomain>
2024-06-13 14:36:19 +0545

    Add encryption technique

--- models/userModel.js
+++ models/userModel.js
@@ -31,4 +31,32 @@ const userSchema = new mongoose.Schema({
 
 },{timestamp:true})
 
+//virtual fields
+userSchema.virtual('password')
+.set(function(password){
+    this._password = password
+    this.salt = uuidv1()
+    this.hashed_password = this.encryptPassword(password)
+})
+
+.get(function(){
+    return this._password
+})
+
+//defining methods
+userSchema.methods={
+    encryptPassword: function(password){
+        if(!password) return ''
+        try{
+            return crypto
+            .Hmac('sha1',this.salt) //encryption garda use huney algorithm 
+            .update(password)
+            .digest('hex')
+        }
+        catch(err){
+            return err
+        }
+    }
+}
+
 module.exports = mongoose.model('User',userSchema)

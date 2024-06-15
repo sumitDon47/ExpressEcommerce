464eb8aa36661ff9cb33c7ae35bdc9fb85b4fc80
Sumit <sumit@pop-os.localdomain>
2024-06-15 17:12:18 +0545

    add sign in user

--- models/userModel.js
+++ models/userModel.js
@@ -1,62 +1,67 @@
-const mongoose = require('mongoose')
-const uuidv1 = require('uuidv1')
-const crypto = require('crypto')
+const mongoose = require("mongoose");
+const uuidv1 = require("uuidv1");
+const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required:true,
-        trim:true
-    },
-    email: {
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
-    },
-    role: {
-        type:Number,
-        default:0
-    },
-    hashed_password:{
-        type:String,
-        required:true
-    },
-    salt:String,
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        role: {
+            type: Number,
+            default: 0,
+        },
+        hashed_password: {
+            type: String,
+            required: true,
+        },
+        salt: String,
 
-    isVerified:{
-        type:Boolean,
-        default:false
-    }
-
-},{timestamp:true})
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamp: true }
+);
 
 //virtual fields
-userSchema.virtual('password')
-.set(function(password){
-    this._password = password
-    this.salt = uuidv1()
-    this.hashed_password = this.encryptPassword(password)
-})
+userSchema
+    .virtual("password")
+    .set(function (password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hashed_password = this.encryptPassword(password);
+    })
 
-.get(function(){
-    return this._password
-})
+    .get(function () {
+        return this._password;
+    });
 
 //defining methods
-userSchema.methods={
-    encryptPassword: function(password){
-        if(!password) return ''
-        try{
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return "";
+        try {
             return crypto
-            .Hmac('sha1',this.salt) //encryption garda use huney algorithm 
-            .update(password)
-            .digest('hex')
+                .Hmac("sha1", this.salt) //encryption garda use huney algorithm
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return err;
         }
-        catch(err){
-            return err
-        }
+    },
+    authenticate: function (plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
     }
-}
+};
 
-module.exports = mongoose.model('User',userSchema)
+module.exports = mongoose.model("User", userSchema);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const {
-  postUser,
-  postEmailConfirmation,
+    postUser,
+    postEmailConfirmation,
+    signIn
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register", postUser);
 router.put("/confirmation/:token", postEmailConfirmation);
+router.post('/signin', signIn);
 
 module.exports = router;

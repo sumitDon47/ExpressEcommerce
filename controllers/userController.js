50f7101b49ef4088d3a049121d473377a9d443e2
Sumit <sumit@pop-os.localdomain>
2024-06-15 14:22:05 +0545

    add utils file for setEmail otp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,28 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
+const Token = require("../models/tokenModel");
+const crypto = require("crypto");
+const sendEmail = require("../utils/setEmail");
 
 //to register user
-exports.postUser = async(req,res)=>{
-    let user = new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    })
-    user = await user.save()
-    if(!user){
-        return res.status(400).json({error:'Unable to create account'})
+exports.postUser = async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  //check if email is already registered
+  user.findOne({ email: user.email }).then(async (data) => {
+    if (data) {
+      return res
+        .status(400)
+        .json({ error: "Email is already registered try new one" });
+    } else {
+      user = await user.save();
+      if (!user) {
+        return res.status(400).json({ error: "Unable to create account" });
+      }
+      res.send(user);
     }
-    res.send(user)
-}
+  });
+};

c666361a7cb55ee10cbaf56bb6ab16bfee5ea91e
Sumit <sumit@pop-os.localdomain>
2024-06-16 17:36:31 +0545

    add validation

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,13 +10,14 @@ const {
     signOut,
     requireSignin
 } = require("../controllers/userController");
+const { userValidation, passwordValidation, validation } = require("../validation/validator");
 const router = express.Router();
 
-router.post("/register", postUser);
+router.post("/register", userValidation, passwordValidation, validation, postUser);
 router.put("/confirmation/:token", postEmailConfirmation);
 router.post('/signin', signIn);
 router.post('/forgetpassword', forgetPassword)
-router.put('/resetpassword/:token', resetPassword)
+router.put('/resetpassword/:token', passwordValidation, validation, resetPassword)
 router.get('/userlist', requireSignin, userList)
 router.get('/userdetails/:id', requireSignin, userDetails)
 router.post('/signout', signOut)
--- validation/validator.js
+++ validation/validator.js
@@ -1,32 +1,50 @@
-const{check,validationResult}=require('express-validator')
+const { check, validationResult } = require('express-validator')
 
 exports.categoryValidation = [
-    check('category_name','category is required').notEmpty()
-    .isLength({min:3}).withMessage('category name must be of at least 3 characters')
+    check('category_name', 'category is required').notEmpty()
+        .isLength({ min: 3 }).withMessage('category name must be of at least 3 characters')
 ]
 
-exports.productValidation=[
-    check('product_name','product name is required').notEmpty()
-    .isLength({min:3}).withMessage('product name must be of at least 3 characters'),
+exports.productValidation = [
+    check('product_name', 'product name is required').notEmpty()
+        .isLength({ min: 3 }).withMessage('product name must be of at least 3 characters'),
 
-    check('product_price','price is required').notEmpty()
-    .isNumeric().withMessage('price must be a numeric value'),
+    check('product_price', 'price is required').notEmpty()
+        .isNumeric().withMessage('price must be a numeric value'),
 
-    check('countInStock','stock is required').notEmpty()
-    .isNumeric().withMessage('stock must be a numeric value'),
+    check('countInStock', 'stock is required').notEmpty()
+        .isNumeric().withMessage('stock must be a numeric value'),
 
-    check('product_description','description is required').notEmpty()
-    .isLength({min:20}).withMessage('Description muct be at least of 20 charactres or more'),
+    check('product_description', 'description is required').notEmpty()
+        .isLength({ min: 20 }).withMessage('Description muct be at least of 20 charactres or more'),
 
-    check('category','category is required').notEmpty()
+    check('category', 'category is required').notEmpty()
+]
+exports.userValidation = [
+    check('name', 'name is required').notEmpty()
+        .isLength({ min: 3 })
+        .withMessage('name must be at least 3 characters'),
+    check('email', 'email is required').notEmpty()
+        .isEmail().withMessage('email forget incorrect')
 ]
 
-exports.validation = (req,res,next)=>{
+exports.passwordValidation = [
+    check('password', 'password is required').notEmpty()
+        .matches(/[a-z]/).withMessage('password must contain one lowercase letter')
+        .matches(/[A-Z]/).withMessage('password must contain one uppercase letter')
+        .matches(/[0-9]/).withMessage('password must contain one numeric value')
+        .matches(/[!@#$^&*()_+]/).withMessage('password must contain one special character')
+        .isLength({ min: 8 }).withMessage('password must be minimum of 8 characters')
+]
+
+
+
+exports.validation = (req, res, next) => {
     const errors = validationResult(req)
-    if(errors.isEmpty()){
+    if (errors.isEmpty()) {
         next()
     }
-    else{
-        return res.status(400).json({error:errors.array()[0].msg})
+    else {
+        return res.status(400).json({ error: errors.array()[0].msg })
     }
 }

6c66315d1412251299a340d14d334cca71788bdd
Sumit <sumit@pop-os.localdomain>
2024-06-17 11:07:37 +0545

    update order status

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -60,10 +60,23 @@ exports.orderDetails = async (req, res) => {
     const order = await Order.findById(req.params.id)
         .populate('user', 'name')
         .populate({
-            path: 'orderItem', populate: {
+            path: 'orderItems', populate: {
                 path: 'product', populate: 'category'
             }
         })
+    if (!order) {
+        return res.status(400).json({ error: 'something went wrong' })
+    }
+    res.send(order)
+}
+
+//UPDATE STATUS
+exports.updateStatus = async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        { status: req.body.status },
+        { new: true }
+    )
     if (!order) {
         return res.status(400).json({ error: 'something went wrong' })
     }
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { postOrder, orderList, orderDetails } = require('../controllers/orderController')
+const { postOrder, orderList, orderDetails, updateStatus } = require('../controllers/orderController')
 const router = express.Router()
 
 router.post('/postorder', postOrder)
 router.get('/orderlist', orderList)
 router.get('/orderdetails/:id', orderDetails)
+router.put('/updatestatus/:id', updateStatus)
 
 module.exports = router
